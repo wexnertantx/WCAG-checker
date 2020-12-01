e5c127e30c0db540d17589642ff2b2cd3c6f8ff6
Robert <2318538n@student.gla.ac.uk>
2020-12-01 17:27:18 +0000

    Adds arguments to program
    Issue: https://stgit.dcs.gla.ac.uk/tp3-2020-CS27/cs27-main/-/issues/31
    
    Command usage: usage: ./main.py [-d | -driver <driver>] <website>
    Available drivers are: chrome and firefox, defaulting to chrome if not specified
    Website argument is mandatory
    
    Example: `./main.py http://stackoverflow.com` (uses default chrome webdriver)
    Example: `./main.py -d firefox http://stackoverflow.com` (uses firefox webdriver)

--- build/main.py
+++ build/main.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
+import sys, getopt, importlib, re, traceback
+import drivers
+
 from os import listdir
 from os.path import isdir, isfile, join
-import importlib
-import re
-import selenium
-
-import drivers, config
+from selenium.common import exceptions as SeleniumExceptions
 
 LOCAL_RULES_PATH = "rules/local"
 EXTERNAL_RULES_PATH = "rules/external"
 
 imported_modules = []
+
+def print_help():
+  print("\nusage: main.py [-d | -driver <driver>] <website>")
+  print("\navailable drivers:")
+  print("\tchrome (default)")
+  print("\tfirefox")
+  sys.exit()
 
 def import_all_local_rules():
   local_module_path = LOCAL_RULES_PATH.replace('/', '.')
@@ -31,21 +37,56 @@ def import_all_external_rules():
   # clone the repo into the external folder and import the module into imported_modules
   pass
 
-import_all_local_rules()
-import_all_external_rules()
-
-def run_rules(website):
-  print("Initiating the webdriver")
-  driver = drivers.get_driver()
+def run_rules(driver, website):
   try:
+    print(f"Initiating the '{driver or ''}' webdriver")
+    driver = drivers.get_driver(driver)
     driver.get(website)
     for module in imported_modules:
-      print(f"\nRunning {module.NAME} v{module.VERSION} accessibility rule on '{website}'")
-      module.run(driver)
-  except Exception as e:
-    raise e
-  finally:
+      try:
+        print(f"\nRunning {module.NAME} v{module.VERSION} accessibility rule on '{website}'")
+        module.run(driver)
+      except Exception as err:
+        print(f"\nUncaught error detected in rule {module.NAME}")
+        print("Make sure you catch all the exceptions inside the rule itself\n")
+        traceback.print_exc(file=sys.stdout)
+  except drivers.DriverError as err:
+    print("\nDriver error:", err, end='\n')
+    print_help()
+  except SeleniumExceptions.InvalidArgumentException as err:
+    print("\nSelenium driver could not start, please check if the website address is valid")
+    print("Error:", err, end='\n')
+  except Exception as err:
+    print("\nSelenium driver could not start, please check the traceback below to find the reason\n")
+    traceback.print_exc(file=sys.stdout)
+
+  if driver != None:
     print("Shutting down the webdriver")
     driver.close()
 
-run_rules(config.get('website'))
+
+def main(argv):
+  try:
+    opts, args = getopt.getopt(argv, "hd:", ["driver="])
+  except getopt.GetoptError:
+    print_help()
+    sys.exit(2)
+  
+  driver = 'chrome'
+  for opt, arg in opts:
+    if opt == '-h':
+      print_help()
+    elif opt in ('-d', '--driver'):
+      driver = arg
+
+  if len(args) == 0:
+    print_help()
+  
+  # Import all rules
+  import_all_local_rules()
+  import_all_external_rules()
+
+  # Run all rules
+  run_rules(driver, args[0])
+
+main(sys.argv[1:])
