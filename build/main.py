196d2f291b76ef083802b693614abe567cb1e5bc
Robert <robertneagu194@gmail.com>
2021-03-18 17:04:38 +0000

    Fixes printing colors on windows

--- build/main.py
+++ build/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import sys, getopt, importlib, re, traceback
 import drivers
+import os
 
+from platform import system
 from os import listdir
 from os.path import isdir, isfile, join
 from selenium.common import exceptions as SeleniumExceptions
@@ -11,31 +13,58 @@ EXTERNAL_RULES_PATH = "rules/external"
 
 imported_modules = []
 
+os.system('color')
+
 COLORS = {
-  "reset": '\u001b[0m',
-  "black": '\u001b[30m',
-  "red": '\u001b[31m',
-  "green": '\u001b[32m',
-  "yellow": '\u001b[33m',
-  "blue": '\u001b[34m',
-  "magenta": '\u001b[35m',
-  "cyan": '\u001b[36m',
-  "white": '\u001b[37m',
-  "bright_black": '\u001b[30;1m',
-  "bright_red": '\u001b[31;1m',
-  "bright_green": '\u001b[32;1m',
-  "bright_yellow": '\u001b[33;1m',
-  "bright_blue": '\u001b[34;1m',
-  "bright_magenta": '\u001b[35;1m',
-  "bright_cyan": '\u001b[36;1m',
-  "bright_white": '\u001b[37;1m',
+  "Windows": {
+    "reset": '\033[0m',
+    "black": '\033[30m',
+    "red": '\033[31m',
+    "green": '\033[32m',
+    "yellow": '\033[33m',
+    "blue": '\033[34m',
+    "magenta": '\033[35m',
+    "cyan": '\033[36m',
+    "white": '\033[37m',
+    "bright_black": '\033[90m',
+    "bright_red": '\033[91m',
+    "bright_green": '\033[92m',
+    "bright_yellow": '\033[93m',
+    "bright_blue": '\033[94m',
+    "bright_magenta": '\033[95m',
+    "bright_cyan": '\033[96m',
+    "bright_white": '\033[97m',
+  },
+  "Unix": {
+    "reset": '\u001b[0m',
+    "black": '\u001b[30m',
+    "red": '\u001b[31m',
+    "green": '\u001b[32m',
+    "yellow": '\u001b[33m',
+    "blue": '\u001b[34m',
+    "magenta": '\u001b[35m',
+    "cyan": '\u001b[36m',
+    "white": '\u001b[37m',
+    "bright_black": '\u001b[30;1m',
+    "bright_red": '\u001b[31;1m',
+    "bright_green": '\u001b[32;1m',
+    "bright_yellow": '\u001b[33;1m',
+    "bright_blue": '\u001b[34;1m',
+    "bright_magenta": '\u001b[35;1m',
+    "bright_cyan": '\u001b[36;1m',
+    "bright_white": '\u001b[37;1m',
+  }
 }
 
+def get_color_code(color):
+  console = 'Windows' if system() == 'Windows' else 'Unix'
+  return COLORS[console][color if color in COLORS[console] else 'reset']
+
 def print_begin_color(color):
-  sys.stdout.write(COLORS[color] if color in COLORS else COLORS['reset'])
+  sys.stdout.write(get_color_code(color))
 
 def print_end_color():
-  sys.stdout.write("\u001b[0m\n")
+  sys.stdout.write(get_color_code('reset'))
 
 def print_color(*args, color='reset', **kwargs):
   print_begin_color(color)
