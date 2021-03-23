c265a0bf94fd24d6f476ff1a194a143ae29441a5
Robert <robertneagu194@gmail.com>
2021-03-23 15:14:56 +0000

    Adds exception catching to css_selector

--- build/util/format.py
+++ build/util/format.py
@@ -1,9 +1,21 @@
+import traceback
+
+# Custom imports
+from util.print import *
+
 ###
 # Returns the element CSS selector as <tag><id><class> format
 # @param element      - Selenium WebDriver WebElement
 ###
 def css_selector(driver, element):
-  elem_tag = driver.execute_script("return arguments[0].tagName.toLowerCase();", element)
-  elem_class = f".{element.get_attribute('class')}" if (element.get_attribute('class') != '') else ''
-  elem_id = f"#{element.get_attribute('id')}" if (element.get_attribute('id') != '') else ''
-  return f"{elem_tag}{elem_id}{elem_class}"
+  try:
+    elem_tag = driver.execute_script("return arguments[0].tagName.toLowerCase();", element)
+    elem_class = f".{element.get_attribute('class')}" if (element.get_attribute('class') != '') else ''
+    elem_id = f"#{element.get_attribute('id')}" if (element.get_attribute('id') != '') else ''
+    return f"{elem_tag}{elem_id}{elem_class}"
+  except Exception:
+    print_begin_color('bright_red')
+    print("\nCSS selector format failed, see stack trace below\n")
+    traceback.print_exc(file=sys.stdout)
+    print_end_color()
+    return f"Unknown HTML element"
