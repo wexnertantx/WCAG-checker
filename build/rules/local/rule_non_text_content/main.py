335cfc2bfc352532813b22cfaef0ba8c3003687c
Robert <robertneagu194@gmail.com>
2021-03-23 13:30:30 +0000

    Refactors rule_non_text_content to match changes

--- build/rules/local/rule_non_text_content/main.py
+++ build/rules/local/rule_non_text_content/main.py
@@ -1,11 +1,16 @@
-import sys
-import io
-from urllib.request import urlretrieve
+import sys, re
 from os import environ, getcwd, path, mkdir
+from shutil import rmtree
+from urllib.request import urlretrieve
+
+# Selenium imports
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from google.cloud import vision
 
+# Custom imports
+import util.errors as CS27Exceptions
+from util.print import *
 import util.format
 
 NAME = "Non-text Content"
@@ -14,119 +19,115 @@ DESCRIPTION = """All non-text content that is presented to the user has a text a
 LINK = "https://www.w3.org/TR/WCAG21/#non-text-content"
 VERSION = 1
 SCRIPT_DIR = path.dirname(path.realpath(__file__))
+SKIP = False
 
 # Set env for google API credentials
 environ["GOOGLE_APPLICATION_CREDENTIALS"] = path.join(getcwd(), "credentials", "google_api.json")
 
 def run(driver):
-  print(f"Printing title from {NAME}: {driver.title}")
+  if (SKIP):
+    raise CS27Exceptions.NoOutput("This rule is flagged to be skipped, check the SKIP flag in your rule!")
 
-  client = vision.ImageAnnotatorClient() # Google Cloud Vision Client
-
+  results_percentage = []
   try:
-    mkdir(path.join(SCRIPT_DIR, "_result"), mode=0o777)
-  except FileExistsError:
-    pass
-  except Exception as err:
-    print(f"[Error] {NAME} rule failed execution:", err, "\n")
-    return
+    client = vision.ImageAnnotatorClient() # Google Cloud Vision Client
 
-  images = driver.find_elements(By.TAG_NAME, "img")
+    tmp_folder = path.join(SCRIPT_DIR, "_tmp")
+    if (not path.exists(tmp_folder)):
+      mkdir(tmp_folder, mode=0o777)
 
-  if len(images):
-    result = {
-      
-      "fail": [],
-      "success": [],
-      "visible": 0,
-    }
-    for i, image in enumerate(images):
-      if image.is_displayed() and image.is_enabled():
-        result['visible'] += 1
-  
-      properties = {
-        "class": f".{image.get_attribute('class')}" if (image.get_attribute('class') != '') else '',
-        "id": f"#{image.get_attribute('id')}" if (image.get_attribute('id') != '') else '',
-        "alt": image.get_attribute('alt'),
-        "src": image.get_attribute('src'),
+    images = driver.find_elements(By.TAG_NAME, "img")
+
+    if len(images):
+      alt_result = {
+        "fail": [],
+        "success": [],
+        "visible": 0,
       }
-      properties["css_selector"] = f"img{properties['id']}{properties['class']}"
-      status = True if (properties['alt'] != '') else False
 
-      if status:
-        #print(f"[Success] ({i}) '{properties['css_selector']}' has an alternative text: '{properties['alt']}'")
-        result['success'].append(properties)
-      else:
-        #print(f"[Fail] ({i}) '{properties['css_selector']}' does not have an alternative text!")
-        result['fail'].append(properties)
-  
-    success_count = len(result['success'])
-    fail_count = len(result['fail'])
-    visible_count = result['visible']
-    images_count = len(images)
-    #print("\n------ Result -----")
-    #print(f"Total images: {images_count} where only {visible_count} are visible")
-    #print(f"Images with an alternative text: {success_count}")
-    #print(f"Images without an alternative text: {fail_count}")
-    print(f"Images with alt-text: {int((success_count/images_count) * 100)}% ({success_count}/{images_count})")
+      for i, image in enumerate(images):
+        if image.is_displayed() and image.is_enabled():
+          alt_result['visible'] += 1
+    
+        properties = {
+          "self": image,
+          "alt": image.get_attribute('alt'),
+          "src": image.get_attribute('src'),
+        }
+        status = True if (properties['alt'] != '') else False
 
+        if status:
+          alt_result['success'].append(properties)
+        else:
+          print_error(f"'{util.format.css_selector(driver, image)}' does not have an alternative text!")
+          alt_result['fail'].append(properties)
+    
+      success_count = len(alt_result['success'])
+      fail_count = len(alt_result['fail'])
+      visible_count = alt_result['visible']
+      images_count = len(images)
 
-    translations={
-      "logo":["logo","icon"]
-    }
+      results_percentage.append(success_count/images_count)
 
-    ## testing
-    try:
-      correct = 0
-      incorrect = 0
-      for i, image in enumerate(result['success']):
-        correctAlt = False
-        percentage = (i / success_count) * 100
+      # Detect image content, check if matches with label
+      # Load custom translations
+      with open(path.join(SCRIPT_DIR, "translations.txt")) as translation_file:
+        translations = {}
+        for line in translation_file:
+          parsed_line = line.split(":")
+          keyword = parsed_line[0]
+          values = parsed_line[1].split(",")
+          translations[keyword] = values
+      
+      # Initialize content result
+      content_result = {
+        "fail": [],
+        "success": [],
+      }
 
-        #print(f"[{percentage:.1f}%] Downloading image", end='\r')
-        file_name, headers = urlretrieve(image['src'], path.join(SCRIPT_DIR, "_result", image['alt'] + '.jpg'))
-
-        #print(f"[{percentage:.1f}%] Opening image", end='\r')
-        with io.open(file_name, 'rb') as f:
-          content = f.read()
+      # Loop through images that have an "alt" attribute
+      for image in alt_result['success']:
+        # Download the image and read its bytes
+        image_file_name, headers = urlretrieve(image['src'], path.join(tmp_folder, f"{image['alt']}.jpg"))
+        with open(image_file_name, 'rb') as image_file:
+          content = image_file.read()
         
         vision_image = vision.Image(content=content)
-        #print(f"[{percentage:.1f}%] Identifying images for alt text corectness", end='\r')
         response = client.label_detection(image=vision_image)
         labels = response.label_annotations
 
-        #print("                                                                          ")
-        #print(f"Labels for '{image['alt']}':")
-        
-        
-
+        regex_labels = []
         for label in labels:
-          labelText = label.description.lower()
+          label_text = label.description.lower()
+          # Append all the translations found for a label into the regex label matcher list
+          if label_text in translations:
+            regex_labels.append('|'.join(translations[label_text]))
+          # Append the label itself into the regex label matcher list
+          regex_labels.append(label_text)
 
-          checks = [labelText]
-          if labelText in translations:
-            checks = translations[labelText]
-          
-          for check in checks:
+        # Compile the regex and test it against the alt image
+        regex_check = re.compile(f"({'|'.join(regex_labels)})", re.IGNORECASE)
+        regex_result = regex_check.findall(image['alt'])
 
-            if check in image['alt'].lower() or image['alt'].lower() in check:
-            
-              correctAlt = True
-        
-
-        if correctAlt:
-          correct+=1
+        if (len(regex_result)):
+          content_result['success'].append(image)
         else:
-          incorrect+=1
-        if i==1:
-          break
-      print(f"{correct/incorrect*100}% of alt text matches image")
-    except Exception as err:
-      print(f"[Error] {NAME} rule failed execution:", err, '\n')
+          content_result['fail'].append(image)
+          print_error(f"'{util.format.css_selector(driver, image['self'])}' alternative text: \"{image['alt']}\" does not match the image content!")
+      
+      success_count = len(content_result['success'])
+      fail_count = len(content_result['fail'])
+      images_count = len(images)
 
+      results_percentage.append(success_count/images_count)
+        
+      if (path.exists(tmp_folder)):
+        rmtree(tmp_folder)
 
+      if (len(results_percentage)):
+        return (sum(results_percentage) / len(results_percentage)) * 100, "% of the page's image content is correct"
 
+  except Exception as err:
+    print_error(f"[Error] {NAME} rule failed execution:", err, '\n')
 
-  # do the required tests for this rule
-  # return something? maybe number of errors, some percentage, yada yada
-  pass
+  raise CS27Exceptions.NoOutput("No output has been returned from this rule!")
