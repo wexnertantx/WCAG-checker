36263a7df78d97e5c957d7a1f3e3ba7d41553483
Robert Neagu <2318538n@student.gla.ac.uk>
2021-03-25 20:45:25 +0000

    Added a feature that runs through all headers and subsequent paragraph
    The codes checks for the similarity or meaning behind the general text
    and passes or fails the cases if the meaning is too different from each
    other.
    
    A template passing rate has been added for testing purposes.

---
+++ build/rules/local/rule_audio_control/main.py
@@ -0,0 +1,136 @@
+from os import path, mkdir
+from shutil import rmtree
+import time
+
+import imagehash
+from PIL import Image
+
+# Selenium imports
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Custom imports
+import util.errors as CS27Exceptions
+from util.print import *
+import util.format as CS27Format
+
+NAME = "Audio Control"
+DESCRIPTION = """If any audio on a Web page plays automatically for more than 3 seconds, either a mechanism is available to pause or stop the audio, or a mechanism is available to control audio volume independently from the overall system volume level."""
+LINK = "https://www.w3.org/TR/WCAG21/#headings-and-labels"
+VERSION = 1
+SCRIPT_DIR = path.dirname(path.realpath(__file__))
+SKIP = False
+
+###
+# Amount of parent levels to look through to find custom media controls, default 2
+#   <div>
+#     <div>
+#        <video></video>
+#     </div>
+#    <div class="controls"></div>          <-- Custom video controls will be found here
+#   </div>
+###
+CONTROLS_PARENT_LIMIT = 2
+
+def run(driver):
+  if (SKIP):
+    raise CS27Exceptions.NoResult("This rule is flagged to be skipped, check the SKIP flag in your rule!")
+
+  time.sleep(2)
+
+  try:
+    tmp_folder = path.join(SCRIPT_DIR, "_tmp")
+    if (not path.exists(tmp_folder)):
+      mkdir(tmp_folder, mode=0o777)
+
+    results_percentage = None
+
+    audio = driver.find_elements(By.TAG_NAME, "audio")
+    video = driver.find_elements(By.TAG_NAME, "video")
+    media = audio + video
+
+    media_autoplay = []
+    media_control = {
+      'fail': [],
+      'success': [],
+    }
+
+    if (len(media)):
+      for element in media:
+        properties = {
+          "self": element,
+          "tag": driver.execute_script("return arguments[0].tagName.toLowerCase();", element),
+          "autoplay": element.get_attribute('autoplay'),
+          "controls": element.get_attribute('controls'),
+        }
+
+        # If video does not have autoplay specified, hard check using video frames
+        if (properties['autoplay'] == 'None' and properties['tag'] == 'video'):
+          first = path.join(tmp_folder, "video-1.png")
+          second = path.join(tmp_folder, "video-2.png")
+          location = element.location
+          size = element.size
+          x = location['x']
+          y = location['y']
+          width = location['x'] + size['width']
+          height = location['y'] + size['height']
+
+          # Save two screenshots with a 4-second interval
+          driver.save_screenshot(first)
+          im = Image.open(first)
+          im = im.crop((int(x), int(y), int(width), int(height)))
+          start_hash = imagehash.average_hash(im)
+          time.sleep(4)
+          driver.save_screenshot(second)
+          im = Image.open(second)
+          im = im.crop((int(x), int(y), int(width), int(height)))
+          end_hash = imagehash.average_hash(im)
+
+          # If the screenshots differ, the video is automatically playing
+          if (start_hash != end_hash):
+            properties['autoplay'] = True
+
+        if (properties['autoplay'] != None):
+          media_autoplay.append(properties)
+    else:
+      return None, "Page has no media"
+
+    if (len(media_autoplay)):
+      for element in media_autoplay:
+        parent = element['self']
+        found = True if (element['controls'] != None) else False
+        level = 0
+        while (found == False and level < CONTROLS_PARENT_LIMIT):
+          parent = driver.execute_script("return arguments[0].parentNode;", parent)
+          controls = list(filter(
+            lambda c: (driver.execute_script("return arguments[0].tagName.toLowerCase();", c) != element['tag']), # Exclude self
+            parent.find_elements(By.XPATH, ".//*[contains(@role, 'button') or contains(translate(@class,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz'), 'control')] | .//button")
+          ))
+
+          # Break out of the loop when controls have been found
+          if (len(controls)):
+            found = True
+          else:
+            level += 1
+
+        if (found):
+          media_control['success'].append(element)
+        else:
+          media_control['fail'].append(element)
+          print_error(f"'{CS27Format.css_selector(driver, element['self'])}' has autoplay enabled but no controls could be found!")
+
+    if (path.exists(tmp_folder)):
+      rmtree(tmp_folder)
+
+    success_count = len(media_control['success'])
+    fail_count = len(media_control['fail'])
+    autoplay_count = len(media_autoplay)
+
+    results_percentage = (success_count/autoplay_count)
+
+    if (results_percentage != None):
+      return results_percentage * 100, "% of the media that have autoplay enabled have controls as well"
+
+  except Exception as err:
+    print_error(f"[Error] {NAME} rule failed execution:", err, '\n')
+    
---
+++ build/rules/local/rule_headings_and_labels/main.py
@@ -0,0 +1,109 @@
+from os import path
+from bs4 import BeautifulSoup
+import time, re
+import spacy
+from subprocess import check_output
+
+# Selenium imports
+from selenium.webdriver.common.by import By
+
+# Custom imports
+import util.errors as CS27Exceptions
+from util.print import *
+import util.format as CS27Format
+
+NAME = "Headings and Labels"
+DESCRIPTION = """Headings and labels describe topic or purpose."""
+LINK = "https://www.w3.org/TR/WCAG21/#headings-and-labels"
+VERSION = 1
+SCRIPT_DIR = path.dirname(path.realpath(__file__))
+SKIP = False
+
+###
+# Amount of text elements to look through in the next sibling to see if the content matches the header, default 2
+#   <h1>Header</h1>
+#   <div flex>
+#     <div>Random text</div>
+#     <div>Some text that matches the header content</div>
+#     --- Script stops checking here, if it doesn't match until here mark header as failed ---
+#     <div>Random text</div>
+#     <div>Random text</div>
+#   </div>
+###
+TEXT_ELEMENTS_LIMIT = 2
+
+def getNextSibling(driver, element):
+  return driver.execute_script("return arguments[0].nextElementSibling", element)
+
+def run(driver):
+  if (SKIP):
+    raise CS27Exceptions.NoResult("This rule is flagged to be skipped, check the SKIP flag in your rule!")
+
+  try:
+    output = check_output('spacy download en_core_web_lg', shell=True).decode()
+    print_end_color()
+
+    results_percentage = None
+
+    nlp = spacy.load("en_core_web_lg")
+    header_elements = driver.find_elements(By.XPATH, '//h1|//h2|//h3|//h4|//h5|//h6')
+    header_content = {
+      'fail': [],
+      'success': [],
+    }
+
+    if (len(header_elements)):
+      for header in header_elements:
+        if (len(header.text) == 0):
+          header_content['fail'].append(header)
+          print_error(f"Header {CS27Format.css_selector(driver, header)} does not have any text")
+          continue
+          
+        sibling = getNextSibling(driver, header)
+        header_tag = CS27Format.getElementTag(driver, header)
+        while (sibling != None and CS27Format.getElementTag(driver, sibling) == header_tag):
+          sibling = getNextSibling(driver, sibling)
+        
+        if (sibling == None):
+          continue
+
+        sibling_text_lines = sibling.text.split('\n')
+        if (len(sibling_text_lines[0]) == 0):
+          header_content['fail'].append(header)
+          print_error(f"Header {CS27Format.css_selector(driver, header)} does not have any content: \"{header.text}\"")
+          continue
+
+        level = 0
+        found = False
+        while (found == False and level < TEXT_ELEMENTS_LIMIT and level < len(sibling_text_lines)):
+          text = sibling_text_lines[level]
+          nlp_header_text = nlp(header.text)
+          nlp_content_text = nlp(text)
+          score = nlp_header_text.similarity(nlp_content_text)
+
+          if (score > 0.65):
+            found = True
+          else:
+            level += 1
+
+        if (found):
+          header_content['success'].append(header)
+        else:
+          header_content['fail'].append(header)
+          print_error(f"Header {CS27Format.css_selector(driver, header)} not descriptive enough in comparison to content: \"{header.text}\"")
+      
+      success_count = len(header_content['success'])
+      fail_count = len(header_content['fail'])
+      header_count = len(header_elements)
+
+      results_percentage = (success_count/header_count)
+
+      if (results_percentage != None):
+        return results_percentage * 100, "% of the headers match their immediate content"
+    else:
+      return None, "Page has no header tags"
+  
+  except Exception as err:
+    print_error(f"[Error] {NAME} rule failed execution:", err, '\n')
+
+  
