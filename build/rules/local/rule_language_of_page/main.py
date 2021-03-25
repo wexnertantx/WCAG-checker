8601c7daf65766b05be1b9991891fb3c5ff94a96
Robert <robertneagu194@gmail.com>
2021-03-25 14:19:12 +0000

    Changes the library that detects the content language

--- build/rules/local/rule_language_of_page/main.py
+++ build/rules/local/rule_language_of_page/main.py
@@ -1,17 +1,15 @@
 from os import path
-import time, re
+import time, re, json
 
 # Selenium imports
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-# Spacy imports
-import spacy
-from spacy.language import Language
-from spacy_langdetect import LanguageDetector
-from spacy.pipeline import Sentencizer
-from spacy.lang.xx import MultiLanguage
-from subprocess import check_output
+import detectlanguage
+detectlanguage.configuration.api_key = "3af1a02a97678b5ab470959a24481d25"
+# TODO: Change the API Key
+# Enable secure mode (SSL) if you are passing sensitive data
+# detectlanguage.configuration.secure = True
 
 # Custom imports
 import util.errors as CS27Exceptions
@@ -24,24 +22,12 @@ VERSION = 1
 SCRIPT_DIR = path.dirname(path.realpath(__file__))
 SKIP = False
 
-@Language.factory("language_detector")
-def create_language_detector(nlp, name):
-  return LanguageDetector(language_detection_function=None)
-
 def run(driver):
   if (SKIP):
     raise CS27Exceptions.NoResult("This rule is flagged to be skipped, check the SKIP flag in your rule!")
   
   try:
-    output = check_output('spacy download xx_ent_wiki_sm', shell=True).decode()
-    print_end_color()
-
     results_percentage = None
-
-    # Load multi language (xx) and create pipelines, sentencizer for sentence boundaries
-    nlp = spacy.load('xx_ent_wiki_sm')
-    nlp.add_pipe('sentencizer')
-    nlp.add_pipe('language_detector', last=True)
     
     html = driver.find_elements(By.TAG_NAME, "html")
     properties = {
@@ -52,22 +38,29 @@ def run(driver):
     lang_results = {
       'fail': [],
       'success': [],
+      'avg_confidence': 0,
     }
 
     if len(properties['lang']):
+      avg_confidence = lang_results['avg_confidence']
       for text in page_text:
         if (len(text) == 0):
           continue
 
-        nlp_text = nlp(text)
-        lang_data = nlp_text._.language
-        lang = lang_data.get('language')
-        score = lang_data.get('score')
-        if lang != properties['lang']:
-          lang_results['fail'].append(text)
-          print_error(f"'{text}' with language ({lang}) failed to match the page language ({properties['lang']})")
-        else:
+        lang_score = detectlanguage.detect(text)[0]
+        lang = lang_score['language']
+        reliable = lang_score['isReliable']
+        confidence = lang_score['confidence']
+        if (lang == properties['lang'] and reliable == True and confidence > 2.5):
           lang_results['success'].append(text)
+        else:
+          lang_results['fail'].append(text)
+          if (lang == properties['lang'] and confidence <= 2.5):
+            print_error(f"'{text}' matches the page language ({lang}) but has a low confidence score of {confidence}")
+          elif (lang == properties['lang'] and reliable == True):
+            print_error(f"'{text}' matches the page language ({lang}) but the result is unreliable")
+          else:
+            print_error(f"'{text}' with language ({lang}) failed to match the page language ({properties['lang']})")
 
       success_count = len(lang_results['success'])
       fail_count = len(lang_results['fail'])
