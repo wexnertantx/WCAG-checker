df713de6e5f22d89f94e06a13cc0fae0b618b23a
Robert <robertneagu194@gmail.com>
2021-04-22 11:10:40 +0100

    Adds fallback for UI

--- build/main.py
+++ build/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, getopt, eel
+import sys, getopt, eel, re
 
 # Custom imports
 from util.print import *
@@ -11,6 +11,15 @@ def eel_close(route, websockets):
   if not websockets:
     print('Shutting down the GUI!')
     sys.exit(0)
+
+def eel_start(mode='chrome'):
+  eel.start('index.html',
+    mode=mode,
+    port=8123,
+    close_callback=eel_close,
+    size=(1800, 1200)
+  )
+
 
 def main(argv):
   try:
@@ -40,7 +49,18 @@ def main(argv):
   if not CS27Gui.is_started():
     CS27Process.analyze_page(driver, args[0])
   else:
-    eel.init('ui/build')
-    eel.start('index.html', port=8123, close_callback=eel_close, size=(1800, 1200))
+    try:
+      eel.init('ui/build')
+      eel_start()
+    except Exception as err:
+      if (len(re.findall(r"Chrome", str(err)))):
+        print_error('Could not find a Google Chrome installation, retrying with Edge')
+        eel_start('edge')
+      if (len(re.findall(r"Edge", str(err)))):
+        print_error('Could not find an Edge installation, retrying with Electron')
+        eel_start('electron')
+      if (len(re.findall(r"Electron", str(err)))):
+        print_error('Could not find an Electron installation, falling back to browser mode')
+        eel_start('browser')
 
 main(sys.argv[1:])
